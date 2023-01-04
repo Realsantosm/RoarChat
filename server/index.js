70d83cd5d16025b81e45e4e58bfa536d0f2dc3bf
Santos <realsantosm@gmail.com>
2023-01-04 16:10:27 +0530

    chorus : All the setup and Register and login

---
+++ server/index.js
@@ -0,0 +1,39 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose')
+
+// import files from ENV and its data
+const URL = process.env.PORT || 4000;
+const URI = process.env.MONGO_URI;
+
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+app.get('/', () => {
+    console.log('I am the HOME....!')
+})
+
+
+// connection to DATABASE
+mongoose.connect(URI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+}).then((data) => {
+    console.log(`Database Connected successfully : ${data.connection.host}`)
+}).catch((err) => {
+    console.log(err.message)
+});
+
+
+// APP Listening
+app.listen(URL, () => {
+    console.log('--------------------------------------')
+    console.log(`Server started on ${URL}`);
+    console.log('--------------------------------------')
+});
