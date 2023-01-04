bf828dc1ab056bf8e844acc73e0a67a2f05cff34
Santos <realsantosm@gmail.com>
2023-01-04 17:06:31 +0530

    fix : fixing register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
+import styled from 'styled-components'
+
 
 const Register = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+    }
   return (
-    <div>
-        i am Register
-    </div>
+    <>
+        <FormContainer>
+            <form onSubmit={(event) => handleSubmit(event)}>
+                <input placeholder='Enter your name' />
+            </form>
+        </FormContainer>
+    </>
   )
 }
 
+const FormContainer = styled.div`
+
+`;
 export default Register
