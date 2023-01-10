ec81bdd12299b28534d6e31d892962432340e5c9
Santos <realsantosm@gmail.com>
2023-01-10 22:00:15 +0530

    updates

---
+++ client/src/pages/Chat.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Chat = () => {
+  return (
+    <div>Chat Page is Ready</div>
+  )
+}
+
+export default Chat
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import styled from 'styled-components'
+// import Login from './Login';
+import Logo from '../assets/logo.svg';
 
 
 const Register = () => {
     const handleSubmit = (event) => {
+        event.preventDefault();
+        alert("form")
+    }
+    const handleChange = (event) => {
         event.preventDefault();
 
     }
@@ -11,14 +18,102 @@ const Register = () => {
     <>
         <FormContainer>
             <form onSubmit={(event) => handleSubmit(event)}>
-                <input placeholder='Enter your name' />
-            </form>
+                <div className="brand">
+                    <img src={Logo} alt="Logo here" />
+                    <h1>RoarKies</h1>
+                </div>
+                <input type="text" placeholder='Username' name='username' onChange={(e) => handleChange(e)} />
+                <input type="email" placeholder='Email' name='email' onChange={(e) => handleChange(e)} />
+                <input type="password" placeholder='Password' name='password' onChange={(e) => handleChange(e)} />
+                <input type="password" placeholder='Confirm Password' name='confirmPassword' onChange={(e) => handleChange(e)} />
+                <button type='submit'>Create User</button>
+                <span>Already have an account? <Link to="/login">Login</Link></span>
+            </form> 
         </FormContainer>
     </>
   )
 }
 
 const FormContainer = styled.div`
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    flex-direction: center;
+    justify-content: center;
+    gap: 1rem;
+    align-items: center;
+    background-color: #f55;
+    .brand {
+        display: flex;
+        align-items: center;
+        gap: 1rem;
+        justify-content: center; 
+        img {
+            height: 5rem;
+        }
+        h1{
+            background: linear-gradient(to right, #f55, orange);
+            //background: linear-gradient(to right, #f32170, #ff6b08, #cf23cf, #eedd44);
+            -webkit-text-fill-color: transparent;
+            -webkit-background-clip: text;
+            font-size: 3rem;
+            font-weight: bold;
+        }
+    }
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 2rem;
+        background-color: #dc2626;
+        border-radius: 2rem;
+        padding: 3rem 5rem;
+        input {
+            background-color: transparent;
+            padding: 1rem;
+            border: 0.2rem solid white;
+            border-radius: 0.5rem;
+            color: white;
+            width: 100%;
+            font-size: 1.5rem;
+            ::placeholder {
+                color: orangered;
+                font-size: 1.5rem;                
+              }
+              &:focus {
+                outline: none;
+                border: 0.2rem solid #f55;
+              }
+        }
+        button {
+            background-color: #f55;
+            padding: .6rem 0rem;
+            border-radius: 0.5rem;
+            border: none;
+            font-size: 1.5rem;
+            font-weight: bold;
+            text-transform: uppercase;
+            &:hover {
+                background-color: red;
+                color: white;
+                transition: 0.2s easy;
+                box-shadow: 3px 2px 3px white;
+                cursor: pointer;
 
+            }
+        }
+        span {
+            color: white;
+            text-transform: uppercase;
+            text-align: center;
+            a{
+                color: black;
+                font-weight: bolder;
+                text-decoration: none;
+                &:hover {
+                    color: yellow;
+                }
+            }
+        }
+    }
 `;
 export default Register
