9a2f5be7f41f39b57b262306a3f716c7bcb77a52
Santos <realsantosm@gmail.com>
2023-02-23 16:50:43 +0530

    Working on Chat File

--- client/src/pages/Chat.jsx
+++ client/src/pages/Chat.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import axios from 'axios';
 
 const Chat = () => {
+  const [contacts, setContacts] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
+  useEffect(async () => {
+    
+    }
+  },[]);
   return (
-    <div>Chat Page is Ready</div>
-  )
-}
+    <Container>
+      <div className="container"></div>
+    </Container>
+  );
+};
 
-export default Chat
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  // background-color: #f55;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    // background-color: #f55;
+
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
+
+export default Chat;
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,68 +1,81 @@
-const User = require("../model/userModel")
-const bcrypt = require('bcrypt');
+const User = require("../model/userModel");
+const bcrypt = require("bcrypt");
 
 module.exports.register = async (req, res, next) => {
-    try {
-    const {username, email, password} = req.body;
-    const usernameCheck = await User.findOne({username});
+  try {
+    const { username, email, password } = req.body;
+    const usernameCheck = await User.findOne({ username });
 
-    if(usernameCheck) {
-        return res.json({msg: "Username is already used", status: false})
+    if (usernameCheck) {
+      return res.json({ msg: "Username is already used", status: false });
     }
-    
-    const emailCheck = await User.findOne({email});
-    if(emailCheck) {
-        return res.json({msg: "Email already exist", status: false});
+
+    const emailCheck = await User.findOne({ email });
+    if (emailCheck) {
+      return res.json({ msg: "Email already exist", status: false });
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = await User.create({
-        email,
-        username,
-        password: hashedPassword,
+      email,
+      username,
+      password: hashedPassword,
     });
     delete user.password;
-    return res.json({status: true, user});
-
-    } catch(err) {
-        next(err);
-    }
-}
-
+    return res.json({ status: true, user });
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports.login = async (req, res, next) => {
-    try {
-    const {username, password} = req.body;
-    const user = await User.findOne({username});
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
 
-    if(!user) {
-        return res.json({msg: "Incorrect username or password", status: false})
+    if (!user) {
+      return res.json({ msg: "Incorrect username or password", status: false });
     }
-    
+
     const isPasswordValid = await bcrypt.compare(password, user.password);
-    if(!isPasswordValid) 
-        return res.json({msg: "Incorrect username or password", status: false});
+    if (!isPasswordValid)
+      return res.json({ msg: "Incorrect username or password", status: false });
     delete user.password;
 
-    return res.json({status: true, user});
-
-    } catch(err) {
-        next(err);
-    }
-}
+    return res.json({ status: true, user });
+  } catch (err) {
+    next(err);
+  }
+};
 
 module.exports.setAvatar = async (req, res, next) => {
-    try {
-        const userId = req.params.id;
-        const avatarImage = req.body.image;
-        const userData = await User.findByIdAndUpdate(userId, {
-           isAvatarImageSet: true,
-           avatarImage, 
-        });
-        return response.json({
-            isSet:userData.isAvatarImageSet, image:userData.isAvatarImageSet,
-        })
-    } catch(err) {
-        next(err)
-    }
-}
+  try {
+    const userId = req.params.id;
+    const avatarImage = req.body.image;
+    const userData = await User.findByIdAndUpdate(userId, {
+      isAvatarImageSet: true,
+      avatarImage,
+    });
+    return response.json({
+      isSet: userData.isAvatarImageSet,
+      image: userData.isAvatarImageSet,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+//* Router for the
+modile.exports.getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({ _id: { $n: req.params.id } }).selected([
+      "email",
+      "username",
+      "avatarImage",
+      "_id",
+    ]);
+    return res.json(users);
+  } catch (err) {
+    next(err);
+  }
+};
